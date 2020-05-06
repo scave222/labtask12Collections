3560c908d07a2dbec7cbfba3f5919444bed2c198
caleb okogbe <okogbecaleb@gmail.com>
2020-05-06 08:38:24 +0100

    hashtable

--- SoftwareEducationalService/Program.cs
+++ SoftwareEducationalService/Program.cs
@@ -7,46 +7,37 @@ namespace SoftwareEducationalService
     {
         static void Main(string[] args)
         {
-            Collections collection1 = new Collections("Chromatography", "Soji", "Chemistry", 4, "Primary");
-            Collections collection2 = new Collections("Lemon Volcano", "Samuel", "Physics", 2, "Higher");
-            Collections collection3 = new Collections("DIY Bouncy Balls", "Ifeanyi", "Integrated Science", 1, "Labs");
-            Collections collection4 = new Collections("Shiny Pennies", "Abayomi", "Chemistry", 1, "Higher");
-            Collections collection5 = new Collections("Regrow Vegetable Scraps", "Gozie", "Agric", 3, "Primary");
-            Collections collection6 = new Collections("DIY Stethoscope", "Buhary", "Physics", 2, "Labs");
-            Collections collection7 = new Collections("Simple Circuit", "Bankole", "Physics", 2, "Primary");
-            Collections collection8 = new Collections("Exploring Density with Salt", "Fatai", "Geography", 4, "Higher");
-            Collections collection9 = new Collections("Rainbow Rubber Eggs", "Michael", "Maths", 3, "Labs");
-            Collections collection10 = new Collections("Magic Rollback Can", "Tolu", "Futher Maths", 1, "Primary");
-            Collections collection11 = new Collections("Bleeding Blossoms", "Grace", "Biology", 5, "Labs");
-            Collections collection12 = new Collections("Teleidoscopes", "Faith", "Integrated Science", 3, "Higher");
-            Collections collection13 = new Collections("Density Tower", "May", "Physics", 5, "Primary");
-            Collections collection14 = new Collections("Magnet Magic", "John", "Chemistry", 4, "Labs");
-            Collections collection15 = new Collections("Overnight Crystals", "Caleb", "Geography", 5, "Higher");
+            string[,] col =new string[15,5] {{"Chromatography", "Soji", "Chemistry", "4", "Primary"},
+                            {"Lemon Volcano", "Samuel", "Physics", "2", "Higher"},
+                            {"DIY Bouncy Balls", "Ifeanyi", "Integrated Science", "1", "Labs"},
+                            {"Shiny Pennies", "Abayomi", "Chemistry", "1", "Higher"},
+                            {"Regrow Vegetable Scraps", "Gozie", "Agric", "3", "Primary"},
+                            {"DIY Stethoscope", "Buhary", "Physics", "2", "Labs"},
+                            {"Simple Circuit", "Bankole", "Physics", "2", "Primary"},
+                            {"Exploring Density with Salt", "Fatai", "Geography", "4", "Higher"},
+                            {"Rainbow Rubber Eggs", "Michael", "Maths", "3", "Labs"},
+                            {"Magic Rollback Can", "Tolu", "Futher Maths", "1", "Primary"},
+                            {"Bleeding Blossoms", "Grace", "Biology", "5", "Labs"},
+                            {"Teleidoscopes", "Faith", "Integrated Science", "3", "Higher"},
+                            {"Density Tower", "May", "Physics", "5", "Primary"},
+                            {"Magnet Magic", "John", "Chemistry", "4", "Labs"},
+                            {"Overnight Crystals", "Caleb", "Geography", "5", "Higher"}};
 
             ArrayList list = new ArrayList(15);
-            list.Add(collection1);
-            list.Add(collection2);
-            list.Add(collection3);
-            list.Add(collection4);
-            list.Add(collection5);
-            list.Add(collection6);
-            list.Add(collection7);
-            list.Add(collection8);
-            list.Add(collection9);
-            list.Add(collection10);
-            list.Add(collection11);
-            list.Add(collection12);
-            list.Add(collection13);
-            list.Add(collection14);
-            list.Add(collection15);
 
-            list.RemoveAt(3);
-            foreach(Collections item in list)
+            for (int i = 0; i < 15; i++)
             {
+                
+                   list.Add($"{col[i,0]},{col[i,1]},{col[i,2]},{col[i,3]},{col[i,4]}"); 
+
+            }
+            list.RemoveAt(3);
+             foreach(var item in list)
+            {
+                //list.Add(item);
                 Console.WriteLine($"{item}");
             }
             Console.WriteLine($"{list.Count}");
-
             Hashtable col1 = new Hashtable();
             col1.Add(60, "58 2000");
             col1.Add(70, "67 2001");
@@ -72,9 +63,14 @@ namespace SoftwareEducationalService
             //     }
             // }
 
-
-            
-
+        //    list.RemoveAt(3);
+        //     foreach(var item in list)
+        //     {
+        //         //list.Add(item);
+        //         Console.WriteLine($"{item}");
+        //     }
+           
+        //     Console.WriteLine($"{list.Count}");
         }
     }
 }
